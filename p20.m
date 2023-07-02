d46826efdbb0deaa586f66ff41c685c47b1cd285
risz1234 <138403783+risz1234@users.noreply.github.com>
2023-07-03 03:01:35 +0530

    Create p20.m

---
+++ p20.m
@@ -0,0 +1,11 @@
+clc; close all; clear all;
+wc1=input('enter the lower cut off freq'); 
+wc2=input('enter the upper cut off freq'); 
+N=input('enter order of the filter');
+a=(N-1)/2;
+n=0:1:N-1;
+hd=(sin(wc1*(n-a+.001))-sin(wc2*(n-a+.001))+sin(pi*(n-a+.001)))./(pi*(n-a+.001))
+wn=hamming(N);
+hn=hd.*wn' 
+freqz(hn,1);
+
